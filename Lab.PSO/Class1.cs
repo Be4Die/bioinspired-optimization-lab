464f30d0a0966501b4bd01dd4ae06e0e78f57db8
Be4Die <misha375375@gmail.com>
2026-01-11 02:09:31 +0400

    orgonize projects

---
+++ Lab.PSO/Class1.cs
@@ -0,0 +1,5 @@
+﻿namespace Lab.PSO;
+
+public class Class1
+{
+}
--- Lab.WebApp/Program.cs
+++ Lab.WebApp/Program.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using WebApp;
+using Lab.WebApp;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
